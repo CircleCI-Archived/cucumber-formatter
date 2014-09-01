17c665bc2a4daf3558fac170a79a5ec1a87c4231
Daniel Woelfel <dwwoelfel@gmail.com>
2014-09-01 15:02:22 -0700

    fix the test

--- features/step_definitions/test_unit_steps.rb
+++ features/step_definitions/test_unit_steps.rb
@@ -1,7 +1,7 @@
 require 'timecop'
 Timecop.scale(36000)
 
-Then /^I can assert that (\w+) == (\w+)$/ do |var_a, var_b|
+Then /^I can assert that (\d+) == (\d+)$/ do |var_a, var_b|
   sleep(1)
-  assert_equal(a, b)
+  assert_equal(var_a, var_b)
 end
