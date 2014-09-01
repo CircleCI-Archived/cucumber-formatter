4d2092bafd258e3215b3576aa99fc40bddf0aecf
Daniel Woelfel <dwwoelfel@gmail.com>
2014-09-01 15:09:07 -0700

    sleep for 2 seconds

--- features/step_definitions/test_unit_steps.rb
+++ features/step_definitions/test_unit_steps.rb
@@ -1,7 +1,9 @@
 require 'timecop'
+# Scale time by a large margin
 Timecop.scale(36000)
 
 Then /^I can assert that (\d+) == (\d+)$/ do |var_a, var_b|
-  sleep(1)
+  # sleep 2 seconds so that we can compare times
+  sleep(2)
   assert_equal(var_a, var_b)
 end
