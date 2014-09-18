20e9bc68e69a0f95de976c1295d2f1443da78fd2
Daniel Woelfel <dwwoelfel@gmail.com>
2014-09-17 17:54:51 -0700

    test against multiple versions

--- test.sh
+++ test.sh
@@ -3,14 +3,43 @@
 set -x
 set -e
 
-# Run once with the unpatched formatter
-bundle exec cucumber --format json --out broken.json
-
 mkdir -p features/support
 cp _circleci_formatter.rb features/support
+mkdir -p gemfiles
 
-# Run once with our formatter
-bundle exec cucumber --format CircleCICucumberFormatter::CircleCIJson --out fixed.json
+# latest version is broken, see https://github.com/cucumber/cucumber/issues/745
+#versions=( "1.3.17" "1.3.0" "git" )
+versions=( "1.3.17" "1.3.0" )
 
-# Check that the times look correct
-./test_output.rb broken.json fixed.json
+for v in "${versions[@]}"
+do
+    echo "Testing with cucumber $v"
+    GEMFILE="gemfiles/Gemfile.$v"
+
+    if [[ "$v" -eq "git" ]]; then
+        v="git: 'git@github.com:cucumber\/cucumber.git'"
+    else
+        v="'$v'"
+    fi
+
+    sed "s/gem 'cucumber'/gem 'cucumber', $v/" Gemfile > $GEMFILE
+
+    export BUNDLE_GEMFILE=$GEMFILE
+
+    bundle install
+
+    rm broken.json; true # only delete if exists
+    rm fixed.json; true
+
+    # Run once with the unpatched formatter
+    bundle exec cucumber --format json --out broken.json
+
+    mkdir -p features/support
+    cp _circleci_formatter.rb features/support
+
+    # Run once with our formatter
+    bundle exec cucumber --format CircleCICucumberFormatter::CircleCIJson --out fixed.json
+
+    # Check that the times look correct
+    ./test_output.rb broken.json fixed.json
+done
--- test_output.rb
+++ test_output.rb
@@ -14,7 +14,7 @@ broken_duration = broken_json[0]["elements"][0]["steps"][0]["result"]["duration"
 fixed_duration = fixed_json[0]["elements"][0]["steps"][0]["result"]["duration"]
 
 if broken_duration < 1*36000*1E9
-  throw "broken duration isn't long enough, the test is probably broken!"
+  throw "broken duration isn't long enough (#{broken_duration}), the test is probably broken!"
 end
 
 if fixed_duration > 20*1E9 # give it a 10x margin (we sleep for 2 secs)
