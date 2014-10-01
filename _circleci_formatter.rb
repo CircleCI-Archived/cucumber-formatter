0db6047af327e84b439d91050c68c00c5a7631c0
Daniel Woelfel <dwwoelfel@gmail.com>
2014-10-01 11:06:19 -0700

    only patch timecop, can't figure out the load order problem

--- _circleci_formatter.rb
+++ _circleci_formatter.rb
@@ -4,20 +4,18 @@ require 'gherkin/formatter/argument'
 require 'gherkin/formatter/json_formatter'
 require 'time'
 
-class Time
-  # This will be loaded before timecop -- support files are loaded first and
-  # are loaded in alphabetical order (hence the _ at the start of the filename)
-  class << self
-    alias_method :original_now, :now
-  end
-end
-
 # Copy of cucumber/formatter/json, with defensive Time.now handling
 module CircleCICucumberFormatter
   class CircleCIJson < Cucumber::Formatter::GherkinFormatterAdapter
     include Cucumber::Formatter::Io
 
     alias_method :original_before_step, :before_step
+
+    def unpatched_time_now
+      if Time.respond_to?(:now_without_mock_time)
+        Time.now_without_mock_time
+      else
+        Time.now
 
     def initialize(runtime, io, options)
       @io = ensure_io(io, "json")
@@ -32,13 +30,13 @@ module CircleCICucumberFormatter
     # override @step_time with un-patched Time.now
     def before_step(step)
       original_before_step(step)
-      @step_time = Time.original_now
+      @step_time = unpatched_time_now
     end
 
     # used for capturing duration, copied from gherkin_formatter_adapter.rb
     # override step_finish with un-patched Time.now
     def after_step(step)
-      step_finish = (Time.original_now - @step_time)
+      step_finish = (unpatched_time_now - @step_time)
       unless @outline and @options and @options[:expand] and not @in_instantiated_scenario
         @gf.append_duration(step_finish)
       end
